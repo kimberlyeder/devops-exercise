a2b31ab43d1359b1c6fb6d8bbb6c1470786eb91f
Kimberly Eder <kimberly.eder@t-online.de>
2025-08-28 19:59:40 +0200

    change test

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ APP_NAME = os.getenv("APP_NAME", "hello-app")
 
 @app.get("/")
 def read_root():
-    return {"app": APP_NAME, "message": "Hello from Kubernetes via ArgoCD!"}
+    return {"app": APP_NAME, "message": "Hello from Kubernetes via ArgoCD! \n Joke: Why did the developer go broke? Because he used up all his cache!"}
 
 @app.get("/healthz")
 def healthz():
